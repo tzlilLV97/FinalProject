3d6c35b7e00d817a161f21cde0d6fe47af0dc139
GuyKoren1 <63463547+GuyKoren1@users.noreply.github.com>
2022-11-06 10:16:23 +0200

    Update Add_data.py

--- Add_data.py
+++ Add_data.py
@@ -1,51 +1,139 @@
-
-from pathlib import Path
 import pandas as pd
-from Bio.Seq import Seq
 from SysEvalOffTarget_src import general_utilities
-
 from Bio import SeqIO
-input_file = general_utilities.HOME_DIR + "GRCh38_latest_genomic.fna"
-output_file = "GRCH38222222.txt"
-genom = ""
-fasta_sequences = SeqIO.parse(open(input_file),'fasta')
-i = 0
-genom_array = [0 for i in range(25)]
-print("kaki")
-dataset_df = pd.read_excel(general_utilities.CHANGE_SEQ_PATH)
-labels = []
-print(dataset_df.columns)
-#moshiko = SeqIO.read(input_file,"fasta")
-if 0:
-    with open(output_file,'w') as out_file:
-        for fasta in fasta_sequences:
-            name, sequence = fasta.id, str(fasta.seq)
-            genom += sequence
-            if name[0:7]=="NC_0000":
-                temp = int(name[7:9])
-                genom_array[temp] = sequence
-            #    genom += sequence
-            #    print(len(genom))
-          #  print(name)
-            if i==26:
-                break
-            if name[0:2]=="NC":
-                i+=1
-            if name[0:2] not in labels:
-                labels.append(name[0:2])
-             #   print(genom)
-               #break
-          #  print(name)
+import warnings
+warnings.filterwarnings("ignore")
 
-#print(len(genom))
-#print(genom)
-print(labels)
-print("What got parsed : ")
-print("Off target : CACACTAATCCTGTCCCCAGAGG")
-print("Target : GTCACCAATCCTGTCCCTAGNGG")
-print(genom[12523844:12523867])
-print("What got parsed : ")
-print("Off target : GGCACCAATGCTGTCCTACCAGG")
-print("Target : GTCACCAATCCTGTCCCTAGNGG")
-print(genom[5332528:5332551])
+def ReverseAndChange(OrigSeq):
+    OrigSeq = OrigSeq[::-1].upper()
+    ReverseSeq = ""
+    for i in range(len(OrigSeq)):
+        if OrigSeq[i] == 'A':
+            ReverseSeq += 'T'
+        elif OrigSeq[i] == 'T':
+            ReverseSeq += 'A'
+        elif OrigSeq[i] == 'G':
+            ReverseSeq += 'C'
+        elif OrigSeq[i] == 'C':
+            ReverseSeq += 'G'
+        else:
+            continue
+    return ReverseSeq
 
+
+def CheckSequence(seq, index):
+    if isinstance(seq, float):
+        # print("found ", seq, "  in index =", index)
+        return True
+
+
+def AddingSixBeforeAndSixAfterToExcel(GenomePath, OrigExcelPath, SixBeforeName, SixAfterName, ChromStartName, ChromEndName, ResultExcelPath):
+    input_file = GenomePath
+    fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
+    dataset_df = pd.read_excel(OrigExcelPath)
+    SixBefore = [0 for i in range(len(dataset_df.index))]
+    dataset_df[SixBeforeName] = SixBefore
+    # dataset_df['All32'] = SixBefore # TODO put in comment
+    dataset_df[SixAfterName] = SixBefore
+    ListOfIds = ["NC_00000" + str(i) for i in range(1, 10)] + ["NC_0000" + str(i) for i in range(10, 25)]
+    ListOfChrs = ["chr" + str(i) for i in range(1, 25)]
+    ChromStartVector = dataset_df[ChromStartName]
+    chromEndVector = dataset_df[ChromEndName]
+
+    for i in fasta_sequences:
+        # print(i.id[0:2])
+        if i.id[0:2] == "NC":
+            if i.id[0:9] in ListOfIds:
+                print("Starting adding 6 nuc before and after in ", ListOfChrs[int(i.id[7:9]) - 1])
+                for j in dataset_df.loc[dataset_df['chrom'] == ListOfChrs[int(i.id[7:9]) - 1]].index:
+                    if CheckSequence(dataset_df['offtarget_sequence'][j], j):
+                        # print(dataset_df.loc[[j]])
+                        continue
+                    TempSeqLen = len(dataset_df['offtarget_sequence'][j])
+                    if dataset_df['strand'][j] == '+':
+                        if dataset_df['offtarget_sequence'][j] != i.seq[ChromStartVector[j]:chromEndVector[j]].upper() and "-" not in dataset_df['offtarget_sequence'][j]:
+                            print(j)
+                            print(dataset_df['offtarget_sequence'][j])
+                            print(i.seq[ChromStartVector[j]:chromEndVector[j]])
+                            raise ValueError
+                        temp35 = i.seq[ChromStartVector[j] - 6:chromEndVector[j] + 6]
+                        dataset_df.loc[j,SixAfterName] = str(temp35[TempSeqLen + 6:TempSeqLen + 12]).upper()
+                        dataset_df.loc[j,SixBeforeName] = str(temp35[0:6]).upper()
+                        # dataset_df['All32'][j] = str(temp35).upper()# TODO put in comment
+                    else:
+                        orig_str = str(i.seq[ChromStartVector[j] - 6:chromEndVector[j] + 6]).upper()
+                        temp35 = ReverseAndChange(orig_str)
+                        if dataset_df['offtarget_sequence'][j] != temp35[6:TempSeqLen + 6] and "-" not in dataset_df['offtarget_sequence'][j]:
+                            print(j)
+                            print(dataset_df['offtarget_sequence'][j])
+                            print(temp35[6:TempSeqLen + 6])
+                            print(orig_str)
+                            raise ValueError
+                        dataset_df.loc[j,SixAfterName] = str(temp35[TempSeqLen + 6:TempSeqLen + 12])
+                        dataset_df.loc[j,SixBeforeName] = str(temp35[0:6])
+                        # dataset_df['All32'][j] = str(temp35)  # TODO put in comment
+
+                if i.id[0:9] == "NC_000023":
+                    for j in dataset_df.loc[dataset_df['chrom'] == 'chrX'].index:
+                        if CheckSequence(dataset_df['offtarget_sequence'][j], j):
+                            continue
+                        TempSeqLen = len(dataset_df['offtarget_sequence'][j])
+                        if dataset_df['strand'][j] == '+':
+                            if dataset_df['offtarget_sequence'][j] != i.seq[ChromStartVector[j]:chromEndVector[j]].upper() and "-" not in dataset_df['offtarget_sequence'][j]:
+                                print(j)
+                                print(dataset_df['offtarget_sequence'][j])
+                                print(i.seq[ChromStartVector[j]:chromEndVector[j]])
+                                raise ValueError
+                            temp35 = i.seq[ChromStartVector[j] - 6:chromEndVector[j] + 6]
+                            dataset_df.loc[j,SixAfterName] = str(temp35[TempSeqLen + 6:TempSeqLen + 12]).upper()
+                            dataset_df.loc[j,SixBeforeName] = str(temp35[0:6]).upper()
+                            # dataset_df['All32'][j] = str(temp35).upper()
+                        else:
+                            orig_str = str(i.seq[ChromStartVector[j] - 6:chromEndVector[j] + 6]).upper()
+                            temp35 = ReverseAndChange(orig_str)
+                            if dataset_df['offtarget_sequence'][j] != temp35[6:TempSeqLen + 6] and "-" not in dataset_df['offtarget_sequence'][j]:
+                                print(j)
+                                print(dataset_df['offtarget_sequence'][j])
+                                print(temp35[6:29])
+                                print(orig_str)
+                                raise ValueError
+                            dataset_df.loc[j,SixAfterName] = str(temp35[TempSeqLen + 6:TempSeqLen + 12])
+                            dataset_df.loc[j,SixBeforeName] = str(temp35[0:6])
+                            # dataset_df['All32'][j] = str(temp35)
+                if i.id[0:9] == "NC_000024":
+                    for j in dataset_df.loc[dataset_df['chrom'] == 'chrY'].index:
+                        if CheckSequence(dataset_df['offtarget_sequence'][j], j):
+                            continue
+                        TempSeqLen = len(dataset_df['offtarget_sequence'][j])
+                        if dataset_df['strand'][j] == '+':
+                            if dataset_df['offtarget_sequence'][j] != i.seq[ChromStartVector[j]:chromEndVector[j]].upper() and "-" not in dataset_df['offtarget_sequence'][j]:
+                                print(j)
+                                print(dataset_df['offtarget_sequence'][j])
+                                print(i.seq[ChromStartVector[j]:chromEndVector[j]])
+                                raise ValueError
+                            temp35 = i.seq[ChromStartVector[j] - 6:chromEndVector[j] + 6]
+                            dataset_df.loc[j,SixAfterName] = str(temp35[TempSeqLen + 6:TempSeqLen + 12]).upper()
+                            dataset_df.loc[j,SixBeforeName] = str(temp35[0:6]).upper()
+                            # dataset_df['All32'][j] = str(temp35).upper()
+                        else:
+                            orig_str = str(i.seq[ChromStartVector[j] - 6:chromEndVector[j] + 6]).upper()
+                            temp35 = ReverseAndChange(orig_str)
+                            if dataset_df['offtarget_sequence'][j] != temp35[6:TempSeqLen + 6] and "-" not in dataset_df['offtarget_sequence'][j]:
+                                print(j)
+                                print(dataset_df['offtarget_sequence'][j])
+                                print(temp35[6:29])
+                                print(orig_str)
+                                raise ValueError
+                            dataset_df.loc[j,SixAfterName] = str(temp35[TempSeqLen + 6:TempSeqLen + 12])
+                            dataset_df.loc[j,SixBeforeName] = str(temp35[0:6])
+                            # dataset_df['All32'][j] = str(temp35)
+                dataset_df.to_excel(ResultExcelPath)
+
+
+AddingSixBeforeAndSixAfterToExcel(GenomePath=general_utilities.HOME_DIR + "GRCh38_latest_genomic.fna", OrigExcelPath=general_utilities.CHANGE_SEQ_PATH,
+                                  SixBeforeName='SixBefore', SixAfterName='SixAfter', ChromStartName='chromStart', ChromEndName='chromEnd',
+                                  ResultExcelPath=general_utilities.DATASETS_PATH + "CHANGE-seqNew.xlsx")
+
+AddingSixBeforeAndSixAfterToExcel(GenomePath=general_utilities.HOME_DIR + "GRCh38_latest_genomic.fna", OrigExcelPath=general_utilities.GUIDE_SEQ_PATH,
+                                  SixBeforeName='SixBefore', SixAfterName='SixAfter', ChromStartName='chromStart', ChromEndName='chromEnd',
+                                  ResultExcelPath=general_utilities.DATASETS_PATH + "GUIDE-seqNew.xlsx")
